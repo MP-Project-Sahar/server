23169e8474f3ab520c2811632fdfa9df12db8a91
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-21 23:41:32 +0300

    connect DB

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
+require("./db");
 
 // Initializations
 const app = express();
 app.use(express.json());
 
-// middlewares
+// Middlewares
 app.use(morgan("dev"));
 // Set security HTTP headers
 app.use(helmet());
-//To allow cross-origin requests
+// To allow cross-origin requests
 app.use(cors());
 
-// set port, listen for requests
+// Set port, listen for requests
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server on ${process.env.PORT}`);
 });
