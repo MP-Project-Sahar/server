bfbcc8d407b83ce8939bbc99c13bc7ebc322c73c
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-22 23:22:37 +0300

    signup

--- routers/controllers/auth.js
+++ routers/controllers/auth.js
@@ -1,4 +1,5 @@
 const userModel = require("./../../db/models/user");
+const bcrypt = require("bcrypt");
 
 const checkEmail = (req, res) => {
   const savedEmail = email.toLowerCase();
@@ -16,4 +17,27 @@ const checkEmail = (req, res) => {
     });
 };
 
-module.exports = { checkEmail };
+const signup = async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  const savedEmail = email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT));
+
+  const newUser = new userModel({
+    email: savedEmail,
+    password: hashedPassword,
+    firstName,
+    lastName
+  });
+
+  newUser
+    .save()
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+module.exports = { checkEmail, signup };
--- routers/routes/auth.js
+++ routers/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { checkEmail } = require("../controllers/auth");
+const { checkEmail, signup } = require("../controllers/auth");
 
 const authRouter = express.Router();
 
 authRouter.post("/checkEmail", checkEmail);
+authRouter.post("/signup", signup);
 
 module.exports = authRouter;
