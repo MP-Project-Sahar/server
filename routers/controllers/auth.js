f33cd1c8b197d77ee403a9995837d2d4a09e01e3
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-22 23:28:39 +0300

    login

--- routers/controllers/auth.js
+++ routers/controllers/auth.js
@@ -40,4 +40,43 @@ const signup = async (req, res) => {
     });
 };
 
-module.exports = { checkEmail, signup };
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  const savedEmail = email.toLowerCase();
+
+  userModel
+    .findOne({ email: savedEmail })
+    .populate("role")
+    .then(async (result) => {
+      if (result) {
+        if (result.email == savedEmail) {
+          const checkedPassword = await bcrypt.compare(
+            password,
+            result.password
+          );
+          if (checkedPassword) {
+            const payload = {
+              id: result._id,
+              role: result.role,
+              isDel: result.isDel
+            };
+            const options = { expiresIn: "1h" };
+            const token = await jwt.sign(payload, secret, options);
+            res.status(200).send({ result, token });
+          } else {
+            return res.status(404).send("Invalid email or password");
+          }
+        } else {
+          res.status(404).send("Invalid email or password");
+        }
+      } else {
+        res.status(404).send("User doesn't exist");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+module.exports = { checkEmail, signup, login };
--- routers/routes/auth.js
+++ routers/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { checkEmail, signup } = require("../controllers/auth");
+const { checkEmail, signup, login } = require("../controllers/auth");
 
 const authRouter = express.Router();
 
 authRouter.post("/checkEmail", checkEmail);
 authRouter.post("/signup", signup);
+authRouter.post("/login", login);
 
 module.exports = authRouter;
