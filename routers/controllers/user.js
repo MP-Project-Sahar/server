a44b0bdf603733d06a115313728a3272e2517e5a
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-24 21:56:18 +0300

    add to favorite

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -203,6 +203,21 @@ const review = (req, res) => {
     });
 };
 
+// Add to favorite
+const addFavorite = (req, res) => {
+  const { user, itemLiked, renterLiked } = req.body;
+
+  const newFavorite = new favoriteModel({ user, itemLiked, renterLiked });
+  newFavorite
+    .save()
+    .then((result) => {
+      res.status(201).send(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
 module.exports = {
   profile,
   favorites,
