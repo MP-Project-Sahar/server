d1bb6b5f2ff5bf8f2805b5efb47196cb7e180704
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-24 19:20:44 +0300

    items

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -1,19 +1,47 @@
 const userModel = require("./../../db/models/user");
+const itemModel = require("./../../db/models/user");
 
+// Show user info. and item 
 const profile = (req, res) => {
   const { id } = req.params;
 
   userModel
     .findOne({
-      _id: id,
+      _id: id, 
       isDel: false
     })
-    .then((result) => {
-      res.status(200).send(result);
+    .then(async (result) => {
+      if(result){
+        const userItems = await itemModel.find({user: id})
+        res.status(200).send({result, userItems});
+      }else{
+        res.status(404).send("Your store is empty");
+      }
     })
     .catch((err) => {
       res.status(400).send(err);
     });
 };
 
-module.exports = { profile };
+// Show items by category
+const items = (req, res) => {
+  const { category } = req.params;
+
+  itemModel
+    .find({
+      category: category,
+      isDel: false
+    })
+    .then((result) => {
+      if(result){
+        res.status(200).send(result);
+      }else{
+        res.status(404).send("empty");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+module.exports = { profile, items };
--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { profile } = require("../controllers/user");
+const { profile, items } = require("../controllers/user");
 
 const userRouter = express.Router();
 
 userRouter.get("/profile/:id", profile);
+userRouter.get("/items/:category", items);
 
 module.exports = userRouter;
